e6f20f19a015a9f7f59d97952ac98b2edeca66ab
Francis Chen <fncis.chen@gmail.com>
2019-03-05 14:18:15 -0800

    organize the ToDoList React Component to prepare for the state changes the Action will be dispatched to the Store's Reducer

--- todo/src/Components/ToDoList.js
+++ todo/src/Components/ToDoList.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import ToDo from "./ToDo"
 
 class ToDoList extends Component {
     render() {
         return(
+
+        <div className="container">
+
             <div className="ToDoList">
+            {
+                this.props.list.map(todo => {
+                    <ToDo todo={todo} completed={this.props.completed}/>
+                })
+            }
             </div>
-        );
+
+
+            <div className="ToDoForm">
+            
+            <button onClick={this.props.removeCompleted}> Remove Completed Items </button>
+
+            </div>
+
+        </div>
+        )
     }
 }
 
-export default ToDoList; 
+const mapStateToProps = state => {
+    return {
+        list: state.list 
+    }
+}
+
+export default connect(mapStateToProps,{add,completed,removeCompleted})(ToDoList); 
