c6ba639673e7c15361522e258d51ac0a400702be
Francis Chen <fncis.chen@gmail.com>
2019-03-05 14:18:40 -0800

    prepared ToDo React Component

---
+++ todo/src/Components/ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+
+    render() {
+        return( 
+            <div className="ToDo" isCompleted={this.props.todo.completed} onClick={this.props.completed}>
+                {this.props.todo.item}
+            </div>
+        )
+    }
+}
+
+export default ToDo;
