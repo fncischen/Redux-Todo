63ae14d5eeff5e3ae726f2798b4add48871ecdf9
Francis Chen <fncis.chen@gmail.com>
2019-03-05 14:52:42 -0800

    made a note to set up styling components to note if an item is completed or note

--- todo/src/Components/ToDo.js
+++ todo/src/Components/ToDo.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+// set up styled components for strikethrough!
+
 class ToDo extends Component {
 
     render() {
         return( 
-            <div className="ToDo" isCompleted={this.props.todo.completed} onClick={this.props.toggle}>
+            <div className="ToDo" completed={this.props.todo.completed} onClick={this.props.toggle}>
                 {this.props.todo.item}
             </div>
         )
