88caf01c2efb46ad45691484f484d78baa6b98f5
Francis Chen <fncis.chen@gmail.com>
2019-03-05 15:29:32 -0800

    filled out cases for our reducer

--- todo/src/Reducers/index.js
+++ todo/src/Reducers/index.js
@@ -1,29 +1,30 @@
 import { TOGGLE, ADD_TODO, REMOVE_COMPLETED } from '../actions';
 
-const a_list = {
-    list: [
+const a_list = [
         {item: "Eat Food", completed: false},
         {item: "Sleep", completed: false},
         {item: "Shopping", completed: false}
-  }
+]
+
+const initialState = {
+    list: a_list
+}
 
 // use Object.Assign()
 // https://learn.lambdaschool.com/fsw/module/recyj0rncjqj5gtxx 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-export default (state = a_list, action) => {
+
+// I'm really in love with Object.assign
+export default (state = initialState, action) => {
     switch(action.type) {
         case TOGGLE:
-            return {
-
-            }
+            return 
         case ADD_TODO: 
-            return {
-
-            }
-        case REMOVE_COMPLETED: 
-            return {
-
-            }
-    }
+            return Object.assign({}, state, {item: action.text, completed: false});
+        case REMOVE_COMPLETED:
+            return state.list.filter(todo => todo.completed == false); 
+        default:
+            return state;
+        }
 
 }
